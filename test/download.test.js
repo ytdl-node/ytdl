a9fca8ecdf612ae405b13ee2fc3156153eb9be33
Rohan Mukherjee <rohanbayern@gmail.com>
2020-03-30 20:08:45 +0530

    feat: Added test files for mocha

---
+++ test/download.test.js
@@ -0,0 +1,12 @@
+const assert = require('assert');
+const fs = require('fs');
+const sendRequest = require('../src/sendRequest');
+
+describe('Download', () => {
+    it('Should run the download function', async () => {
+        assert.equal(await sendRequest('download'), undefined);
+    });
+    it('Should download in data directory', () => {
+        assert.equal(fs.existsSync('./data/download'), true);
+    });
+});
