bc87c13a2b0ff12dc1b3b8533ff1ca55ea28a724
Rohan Mukherjee <rohanbayern@gmail.com>
2020-03-31 18:58:29 +0530

    feat: Added logger, improved test

---
+++ src/utils/logger.js
@@ -0,0 +1,13 @@
+const winston = require('winston');
+
+const logger = winston.createLogger({
+    transports: [
+        new winston.transports.Console(),
+    ],
+    format: winston.format.combine(
+        winston.format.colorize(),
+        winston.format.simple(),
+    ),
+});
+
+module.exports = logger;
--- test/lint.test.js
+++ test/lint.test.js
@@ -6,7 +6,18 @@ const cli = new CLIEngine({ useEslintrc: true });
 const report = cli.executeOnFiles(['src/', 'test/']);
 
 describe('Lint', () => {
-    it('Check for linting errors', () => {
-        assert.equal(report.errorCount, 0);
+    it('Check for linting errors', (done) => {
+        try {
+            assert.equal(report.errorCount, 0);
+            done();
+        } catch (err) {
+            const errorFiles = [];
+            report.results.forEach((i) => {
+                if (i.errorCount !== 0) {
+                    errorFiles.push(`\n\t${i.filePath}`);
+                }
+            });
+            throw new Error(errorFiles);
+        }
     });
 });
