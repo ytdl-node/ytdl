a8f735e6aa02849c2f703372e9e6dfa157440224
Rohan Mukherjee <rohanbayern@gmail.com>
2020-03-31 18:06:58 +0530

    Feat: npm run lint now checks test/ too

--- test/lint.test.js
+++ test/lint.test.js
@@ -3,7 +3,7 @@ const assert = require('assert');
 
 const cli = new CLIEngine({ useEslintrc: true });
 
-const report = cli.executeOnFiles(['src/']);
+const report = cli.executeOnFiles(['src/', 'test/']);
 
 describe('Lint', () => {
     it('Check for linting errors', () => {
