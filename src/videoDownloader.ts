724499bfa1a4d6a0765d60cac7e52799c2e9f762
Rohan Mukherjee <rohanbayern@gmail.com>
2020-04-04 16:39:56 +0530

    Fix: Linting

--- src/videoDownloader.ts
+++ src/videoDownloader.ts
@@ -4,6 +4,32 @@ import fs from 'fs';
 
 import VideoInfo from './models/VideoInfo';
 
+export default async function download(urls: string[], filename: string) {
+    const host = urls[0].split('/videoplayback')[0].split('https://')[1];
+    console.log(host);
+    axios({
+        method: 'get',
+        url: urls[0],
+        responseType: 'stream',
+        headers: {
+            Accept: '*/*',
+            'Accept-Encoding': 'gzip, deflate, br',
+            'Accept-Language': 'en-US,en;q=0.9,hi;q=0.8,de-DE;q=0.7,de;q=0.6,bn;q=0.5,la;q=0.4',
+            Connection: 'keep-alive',
+            Host: host,
+            Origin: 'https://www.youtube.com',
+            Referer: 'https://www.youtube.com/',
+            'Sec-Fetch-Dest': 'empty',
+            'Sec-Fetch-Mode': 'cors',
+            'Sec-Fetch-Site': 'cross-site',
+            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36',
+        },
+    })
+        .then((response) => {
+            response.data.pipe(fs.createWriteStream(filename));
+        });
+}
+
 export function fetchLinks(videoInfo: VideoInfo, qualityLabel: string, filename?: string) {
     const urls: Array<string> = [];
 
@@ -20,29 +46,7 @@ export function fetchLinks(videoInfo: VideoInfo, qualityLabel: string, filename?
         }
     });
 
-    const host = urls[0].split('/videoplayback')[0].split('https://')[1];
-    console.log(host);
-    axios({
-        method: 'get',
-        url: urls[0],
-        responseType: 'stream',
-        headers: {
-            Accept: '*/*',
-            'Accept-Encoding': 'gzip, deflate, br',
-            'Accept-Language': 'en-US,en;q=0.9,hi;q=0.8,de-DE;q=0.7,de;q=0.6,bn;q=0.5,la;q=0.4',
-            Connection: 'keep-alive',
-            Host: host,
-            Origin: 'https://www.youtube.com',
-            Referer: 'https://www.youtube.com/',
-            'Sec-Fetch-Dest': 'empty',
-            'Sec-Fetch-Mode': 'cors',
-            'Sec-Fetch-Site': 'cross-site',
-            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36',
-        },
-    })
-        .then((response) => {
-            response.data.pipe(fs.createWriteStream('./data/video2.mp4'));
-        });
+    download(urls, 'video3.mp4');
 }
 
 export function fetchAudioStream(videoInfo: VideoInfo, quality: string) {
@@ -55,31 +59,5 @@ export function fetchAudioStream(videoInfo: VideoInfo, quality: string) {
         }
     });
 
-    const host = urls[0].split('/videoplayback')[0].split('https://')[1];
-    console.log(host);
-    axios({
-        method: 'get',
-        url: urls[0],
-        responseType: 'stream',
-        headers: {
-            Accept: '*/*',
-            'Accept-Encoding': 'gzip, deflate, br',
-            'Accept-Language': 'en-US,en;q=0.9,hi;q=0.8,de-DE;q=0.7,de;q=0.6,bn;q=0.5,la;q=0.4',
-            Connection: 'keep-alive',
-            Host: host,
-            Origin: 'https://www.youtube.com',
-            Referer: 'https://www.youtube.com/',
-            'Sec-Fetch-Dest': 'empty',
-            'Sec-Fetch-Mode': 'cors',
-            'Sec-Fetch-Site': 'cross-site',
-            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36',
-        },
-    })
-        .then((response) => {
-            response.data.pipe(fs.createWriteStream('./data/audio.mp4'));
-        });
-}
-
-export default async function download(links: string[]) {
-    console.log(links);
+    download(urls, 'audio.mp4');
 }
