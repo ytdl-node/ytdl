627579d24c399b2a6c27d2b81ab1b14692c34734
Rohan Mukherjee <rohanbayern@gmail.com>
2020-04-15 00:58:22 +0530

    feat: deleteFile checks if exists

--- src/utils/deleteFile.ts
+++ src/utils/deleteFile.ts
@@ -1,10 +1,17 @@
 import fs from 'fs';
 
 export default async function deleteFile(filename: string) {
+    const path = `./data/${filename}`;
     return new Promise((resolve, reject) => {
-        fs.unlink(`./data/${filename}`, (err) => {
-            if (err) reject(err);
-            else resolve();
+        fs.stat(path, (exists) => {
+            if (exists == null) {
+                fs.unlink(path, (err) => {
+                    if (err) reject(err);
+                    else resolve();
+                });
+            } else if (exists.code === 'ENOENT') {
+                resolve();
+            }
         });
     });
 }
