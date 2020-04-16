d6e0a402e7be8a6c93ced1966a94f6814083d741
Rohan Mukherjee <rohanbayern@gmail.com>
2020-04-16 21:24:00 +0530

    feat: Test files modified

--- test/download.test.ts
+++ test/download.test.ts
@@ -1,8 +1,27 @@
 /* eslint-disable @typescript-eslint/no-unused-expressions */
 import { expect } from 'chai';
+import ytdl from '../src/index';
+
+async function videoDownloader() {
+    const video = await ytdl('https://www.youtube.com/watch?v=fJ9rUzIMcZQ');
+    await video.download('360p', 'data/ytdl.mp4');
+    return true;
+}
+
+async function videoDownloaderByItag() {
+    const video = await ytdl('https://www.youtube.com/watch?v=fJ9rUzIMcZQ');
+    await video.downloadByItag(396, 'data/ytdlItag.mp4');
+    return true;
+}
 
 describe('Download', () => {
-    it('Should run the download function', async () => {
-        expect(true).to.be.true;
+    it('Should download the sample video', async (done) => {
+        expect(await videoDownloader()).to.be.true;
+        done();
+    });
+
+    it('Should be able to download by itag', async (done) => {
+        expect(await videoDownloaderByItag()).to.be.true;
+        done();
     });
 });
