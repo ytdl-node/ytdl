4863a3680d25edf1761c4ff771cad7d7a2c26c87
Rohan Mukherjee <rohanbayern@gmail.com>
2020-04-21 04:30:24 +0530

    feat: Modified function init, don't export irrelevant features

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,16 @@
-import VideoData from './videoData';
+import Ytdl from './ytdl';
 
 import mergeStreams from './utils/mergeStreams';
 import getDownloadLink from './utils/getDownloadLink';
-import dumpVideoInfo, { dumpToFile } from './utils/jsonDump';
 
-export default function init(link: string): Promise<VideoData> {
-    return VideoData.fromLink(link);
+export default function init(link: string): Promise<Ytdl> {
+    return Ytdl.init(link);
 }
 
-export * from './cli';
-export { fetch, fetchContent, fetchContentByItag } from './downloader';
+// export * from './cli';
 
 export {
     mergeStreams,
     getDownloadLink,
-    dumpVideoInfo,
-    dumpToFile,
     init,
 };
