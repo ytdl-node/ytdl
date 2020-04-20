e6873a1b7c78e3ff344ec0c2b36e121ed21c484c
Rohan Mukherjee <rohanbayern@gmail.com>
2020-04-21 05:00:34 +0530

    Feat: Export CLI and fetch

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,17 @@ import Ytdl from './ytdl';
 
 import mergeStreams from './utils/mergeStreams';
 import getDownloadLink from './utils/getDownloadLink';
+import fetch from './videoDownloader';
 
 export default function init(link: string): Promise<Ytdl> {
     return Ytdl.init(link);
 }
 
-// export * from './cli';
+export * from './cli';
 
 export {
     mergeStreams,
     getDownloadLink,
+    fetch,
     init,
 };
