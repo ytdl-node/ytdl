dc29fc92b3bac9881c9530d32bf56064a464f7b3
Rohan Mukherjee <rohanbayern@gmail.com>
2020-04-22 01:20:06 +0530

    feat: CreateLogger function with param level added

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,8 +1,25 @@
 import winston from 'winston';
 
+export function createLogger(level: string) {
+    const logger = winston.createLogger({
+        transports: [
+            new winston.transports.Console({
+                level,
+            }),
+        ],
+        format: winston.format.combine(
+            winston.format.colorize(),
+            winston.format.simple(),
+        ),
+    });
+    return logger;
+}
+
 const logger = winston.createLogger({
     transports: [
-        new winston.transports.Console(),
+        new winston.transports.Console({
+            level: 'error',
+        }),
     ],
     format: winston.format.combine(
         winston.format.colorize(),
