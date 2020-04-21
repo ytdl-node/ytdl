05e22a8d31bd8c8de263d3632822934645a11ede
Rohan Mukherjee <rohanbayern@gmail.com>
2020-04-22 01:39:32 +0530

    feat: Logger throws error on invalid log level

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,6 +1,23 @@
 import winston from 'winston';
 
 export function createLogger(level: string) {
+    type level = {
+        [key: string]: number
+    };
+    const levels: level = {
+        error: 0,
+        warn: 1,
+        info: 2,
+        http: 3,
+        verbose: 4,
+        debug: 5,
+        silly: 6,
+    };
+
+    if (!levels[level]) {
+        throw new Error('Invalid logging level.');
+    }
+
     const logger = winston.createLogger({
         transports: [
             new winston.transports.Console({
