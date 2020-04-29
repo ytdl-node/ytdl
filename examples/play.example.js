447d30363e910a87e12a6739f4d5be42737b39f8
Rohan Mukherjee <rohanbayern@gmail.com>
2020-04-30 04:50:54 +0530

    feat: Modified player example to log the player

--- examples/play.example.js
+++ examples/play.example.js
@@ -1,11 +1,23 @@
 const ytdl = require('..'); // require('@ytdl/ytdl');
 
 /**
- * This function players audio from 'https://www.youtube.com/watch?v=A7ry4cx6HfY' on locally installed 'mplayer'
+ * This function players audio from 'https://www.youtube.com/watch?v=A7ry4cx6HfY' on 'ytdl-mp3' player;
  */
 async function play() {
     const video = await ytdl.init('https://www.youtube.com/watch?v=A7ry4cx6HfY');
-    video.play('any', { audioOnly: true }, 'mplayer');
+    const player = await video.play('any', { audioOnly: true });
+    console.log(`Player: ${player.player}`);
 }
 
 play();
+
+/**
+ * This function players audio from 'https://www.youtube.com/watch?v=A7ry4cx6HfY' on 'mplayer';
+ */
+async function playLocal() {
+    const video = await ytdl.init('https://www.youtube.com/watch?v=A7ry4cx6HfY');
+    const player = await video.play('any', { audioOnly: true }, 'mplayer');
+    console.log(`Player: ${player.player}`);
+}
+
+playLocal();
