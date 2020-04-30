2b171e775725beb83aa854ad116c3fe57c2cf947
Rohan Mukherjee <rohanbayern@gmail.com>
2020-05-01 02:57:53 +0530

    feat: ProgressBar accepts logger, if level above info, shows progressBar

--- src/utils/progressBar.ts
+++ src/utils/progressBar.ts
@@ -1,16 +1,21 @@
+import { Logger } from 'winston';
+
 export default class ProgressBar {
     total: number;
 
     current: number;
 
-    private readonly barLength: number;
-
     readonly preText: string;
 
-    constructor(total: number, preText?: string) {
+    private readonly barLength: number;
+
+    private readonly logger: Logger;
+
+    constructor(total: number, preText?: string, logger?: Logger) {
         this.total = total;
         this.current = 0;
         this.preText = preText || 'Current progress';
+        this.logger = logger;
 
         const { columns } = process.stdout;
         this.barLength = columns <= 75 ? columns - 30 : 45;
@@ -19,7 +24,7 @@ export default class ProgressBar {
     }
 
     public update(current: number): string {
-        this.current = current;
+        this.current += current;
         const currentProgress = this.current / this.total;
         const filledBarLength: number = Number((currentProgress * this.barLength).toFixed(0));
         const emptyBarLength = this.barLength - filledBarLength;
@@ -42,9 +47,23 @@ export default class ProgressBar {
 
         process.stdout.clearLine(0);
         process.stdout.cursorTo(0);
-        process.stdout.write(
-            `${this.preText}: [${filledBar}${emptyBar}] | ${percentageProgress}%`,
-        );
+
+        if (this.logger) {
+            const { error, warn, ...acceptedLevels } = this.logger.levels;
+            if (acceptedLevels[this.logger.level]) {
+                if (percentageProgress === '0.00') {
+                    process.stdout.write('\n');
+                }
+
+                process.stdout.write(
+                    `${this.preText}: [${filledBar}${emptyBar}] | ${percentageProgress}%`,
+                );
+
+                if (percentageProgress === '100.00') {
+                    process.stdout.write('\n\n');
+                }
+            }
+        }
     }
 
     private static getBar(length: number, char: string): string {
