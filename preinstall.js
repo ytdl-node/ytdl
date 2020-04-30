c31fba14cc0761b167e7a9fbd8d35e36300d4607
Rohan Mukherjee <rohanbayern@gmail.com>
2020-04-30 06:16:13 +0530

    feat: Added preinstall prompt for asoundlib.h

---
+++ preinstall.js
@@ -0,0 +1,17 @@
+const { execSync } = require('child_process');
+
+if (process.platform === 'linux') {
+    let exitStatus;
+    try {
+        execSync('echo "#include <alsa/asoundlib.h>" | gcc -H -o /dev/null -x c - 2>&1 | head -n1').toString();
+        exitStatus = 0;
+    } catch (err) {
+        exitStatus = err.status;
+    }
+
+    if (exitStatus !== 0) {
+        console.error(`'alsa/asoundlib.h' is absent. For Ubuntu/Debian operating systems, install using the following command:
+        \nsudo apt-get install libasound2-dev\n`);
+        process.exit(1);
+    }
+}
